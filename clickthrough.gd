6723720c152f7561b7da39dd05430ab1cd218263
Lee Cummings <kierstenlcummings@gmail.com>
2025-04-05 12:41:44 -0400

    add clickthrough

--- clickthrough.gd
+++ clickthrough.gd
@@ -5,13 +5,13 @@ extends Node2D
 func _process(delta):
 	set_passthrough($sitting) # change this if sprite name changes
 
-func set_passthrough(sprite: Sprite2D):
-	var texture_center: Vector2 = sprite.texture.get_size() / 2
+func set_passthrough(sprite: AnimatedSprite2D):
+	var texture_center: Vector2 = Vector2(sprite.sprite_frames.get_frame_texture(sprite.animation, sprite.frame).get_size()) * 0.5*sprite.scale.x
 	var texture_corners: PackedVector2Array = [
 		sprite.global_position + texture_center * Vector2(-1, -1), # Top left corner
 		sprite.global_position + texture_center * Vector2(1, -1), # Top right corner
 		sprite.global_position + texture_center * Vector2(1 , 1), # Bottom right corner
 		sprite.global_position + texture_center * Vector2(-1 ,1) # Bottom left corner
 	]
-
+	
 	DisplayServer.window_set_mouse_passthrough(texture_corners)
