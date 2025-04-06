b70e9e33e0a7c59cf73334a2902fb46632be2f7e
Lee Cummings <kierstenlcummings@gmail.com>
2025-04-05 21:51:54 -0400

    fixes

--- Collision.cs
+++ Collision.cs
@@ -7,7 +7,7 @@ public partial class Collision : CollisionShape2D
 	private int y;
 	public override void _Process(double delta){
 		AnimatedSprite2D cat = GetNode<AnimatedSprite2D>("../Cat");
-		this.Position = cat.Position;
+		this.GlobalPosition = cat.GlobalPosition;
 		//GD.Print("weh");
 	}
 }
